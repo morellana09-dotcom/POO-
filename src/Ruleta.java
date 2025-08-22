91a50081abb003d6a8194001c4a764488ed50a08
morellana09-dotcom <m.orellana09@ufromail.cl>
2025-08-22 11:25:11 -0400

    Empezar de 0

---
+++ src/Ruleta.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+public class Ruleta {
+    public static final int MAX_HISTORIAL = 100;
+    public static int[] historialNumeros = new int[MAX_HISTORIAL];
+    public static int[] historialApuestas = new int[MAX_HISTORIAL];
+    public static boolean[] historialAciertos = new boolean[MAX_HISTORIAL];
+    public static int historialSize = 0;
+    public static Random rng = new Random();
+    public static int[] numerosRojos =
+            {1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36};
+}
+
+
