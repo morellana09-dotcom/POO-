b396ca50662a8bfc18cb0ec95af25722aebf5be9
morellana09-dotcom <m.orellana09@ufromail.cl>
2025-09-14 13:06:35 -0300

    Avanze#2: - Enchulamos la maquina

--- src/Ventanas/VentanaLogin.java
+++ src/Ventanas/VentanaLogin.java
@@ -1,7 +1,8 @@
 package Ventanas;
 
+import Logica.Login;
+
 import javax.swing.*;
-import javax.swing.plaf.PanelUI;
 import java.awt.*;
 
 public class VentanaLogin {
@@ -12,36 +13,68 @@ public class VentanaLogin {
     private final JPasswordField txtClave = new JPasswordField("");
     private final JButton btnIngresar = new JButton("Ingresar");
 
+
     public VentanaLogin() {
-        configuracionVentanaLogin();
-
-    }
-
-    public void botonesLogin() {
+        frameLogin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frameLogin.setSize(700, 300);
+        frameLogin.setLocationRelativeTo(null);
+        frameLogin.setLayout(null);
+        frameLogin.getContentPane().setBackground(new Color(50,50,50));
+        frameLogin.getRootPane().setDefaultButton(btnIngresar);
 
     }
 
     public void labelsLogin() {
-        lblUsuario.setBounds(1,1,1,1);
+        lblUsuario.setBounds(290, 30, 120, 25);
+        lblUsuario.setForeground(Color.WHITE);
+        lblUsuario.setFont(new Font("Arial", Font.BOLD, 25));
+        lblClave.setBounds(300,100,120,25);
+        lblClave.setForeground(Color.WHITE);
+        lblClave.setFont(new Font("Arial", Font.BOLD, 25));
     }
 
     public void textosLogin() {
-
+        txtUsuario.setBounds(270,60,130,25);
+        txtUsuario.setHorizontalAlignment(JTextField.CENTER);
+        txtClave.setBounds(270,130,130,25);
+        txtClave.setHorizontalAlignment(JTextField.CENTER);
     }
 
-    public void configuracionVentanaLogin() {
-        frameLogin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frameLogin.setSize(700, 200);
-        frameLogin.setLocationRelativeTo(null);
-        frameLogin.setLayout(null);
-        frameLogin.getContentPane().setBackground(new Color(50,50,50));
+    public void botonesLogin() {
+        btnIngresar.setBounds(290,170,90,25);
+        btnIngresar.setBackground(new Color(39, 207, 96));
+        btnIngresar.setForeground(Color.WHITE);
+        btnIngresar.addActionListener(e -> verificacionLogin());
+
+
     }
 
     public void agregarVentanaLogin() {
-
+        frameLogin.add(lblUsuario);
+        frameLogin.add(txtUsuario);
+        frameLogin.add(lblClave);
+        frameLogin.add(txtClave);
+        frameLogin.add(btnIngresar);
     }
 
     public void mostrarVentanaLogin() {
+        labelsLogin();
+        textosLogin();
+        botonesLogin();
+        agregarVentanaLogin();
         frameLogin.setVisible(true);
+    }
+
+    public void verificacionLogin() {
+        Login login = new Login();
+        String u = txtUsuario.getText().trim();
+        String p = txtClave.getText().trim();
+        String nombre = login.validarCredenciales(u,p);
+
+        if (nombre.isEmpty()){
+            JOptionPane.showMessageDialog(frameLogin,"Username y/o contraseña Incorrecta","intente nuevamente", JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(frameLogin, "Bienvenuti " + nombre, "Bienvenido", JOptionPane.INFORMATION_MESSAGE);
+        }
     }
 }
